6f62c8619eefeef7b5160ac7dd2f2be3c57abeb7
Basudev-Pokharel <basudevpokharelfin@gmail.com>
2026-02-11 13:01:23 +0200

    chores: add supabase things in database config

--- config/database.php
+++ config/database.php
@@ -94,8 +94,9 @@ return [
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'public',
-            'sslmode' => 'prefer',
+            'schema' => 'laravel',
+            'search_path' => 'public, laravel',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
